e54e224dd74f5c7856e682b09ecd46a5a8723459
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-08-06 15:28:40 +0800

    [UPDATE] implementasi simulasi process komputasi tinggi tanpa background process

--- app/src/main/java/id/one/backgroundthread/MainActivity.kt
+++ app/src/main/java/id/one/backgroundthread/MainActivity.kt
@@ -2,10 +2,32 @@ package id.one.backgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart: Button = findViewById(R.id.btn_start)
+        val tvStatus: TextView = findViewById(R.id.tv_status)
+
+        btnStart.setOnClickListener {
+            try {
+                // simulate process compressing
+                for(i in 0..10){
+                    Thread.sleep(500)
+                    val persentage =  i * 10
+                    if(persentage == 100){
+                        tvStatus.setText(R.string.task_completed)
+                    }else{
+                        tvStatus.text = String.format(getString(R.string.compressing),persentage)
+                    }
+                }
+            } catch (e: InterruptedException){
+                e.printStackTrace()
+            }
+        }
     }
 }
