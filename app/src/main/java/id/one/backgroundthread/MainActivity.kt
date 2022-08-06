0b7bc2e36cb0b72eb0d3cb5e44629ca699cbc5c4
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-08-06 15:37:26 +0800

    [UPDATE] implementasi Thread baru dan handler untuk update hasil ke UI Thread dari background thread/process

--- app/src/main/java/id/one/backgroundthread/MainActivity.kt
+++ app/src/main/java/id/one/backgroundthread/MainActivity.kt
@@ -2,8 +2,11 @@ package id.one.backgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import java.util.concurrent.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,20 +16,32 @@ class MainActivity : AppCompatActivity() {
         val btnStart: Button = findViewById(R.id.btn_start)
         val tvStatus: TextView = findViewById(R.id.tv_status)
 
+        // Membuat sebuah thread baru
+        val executor = Executors.newSingleThreadExecutor()
+        // Membuat handler untuk update hasil ke UI dari background thread/process
+        val handler = Handler(Looper.getMainLooper()) // getMainLooper agar handler di jalankan di main/UI thread
+
         btnStart.setOnClickListener {
-            try {
-                // simulate process compressing
-                for(i in 0..10){
-                    Thread.sleep(500)
-                    val persentage =  i * 10
-                    if(persentage == 100){
-                        tvStatus.setText(R.string.task_completed)
-                    }else{
-                        tvStatus.text = String.format(getString(R.string.compressing),persentage)
+            executor.execute {
+                try {
+                    // simulate process compressing
+                    for(i in 0..10){
+                        Thread.sleep(500)
+                        val persentage =  i * 10
+
+                        handler.post {
+                            // update ui in main thread
+                            if(persentage == 100){
+                                tvStatus.setText(R.string.task_completed)
+                            }else{
+                                tvStatus.text = String.format(getString(R.string.compressing),persentage)
+                            }
+                        }
+
                     }
+                } catch (e: InterruptedException){
+                    e.printStackTrace()
                 }
-            } catch (e: InterruptedException){
-                e.printStackTrace()
             }
         }
     }
