b7149a5da00983ddac3b647a4782e4b21ac2c7fc
Arwan Prianto Mangidi <arwanpriantomangidi@gmail.com>
2022-08-06 16:13:01 +0800

    [UPDATE] implement process with Background Thread and without Background Thread (only Main/UI Thread)

--- app/src/main/java/id/one/backgroundthread/MainActivity.kt
+++ app/src/main/java/id/one/backgroundthread/MainActivity.kt
@@ -6,6 +6,11 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +19,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val btnStart: Button = findViewById(R.id.btn_start)
+        val btnStartWithoutBackgroud: Button = findViewById(R.id.btn_start_without_background)
+        val btnStartCoroutine: Button = findViewById(R.id.btn_start_coroutine)
         val tvStatus: TextView = findViewById(R.id.tv_status)
+
+        // WITHOUT BACKGROUND THREAD, ONLY MAIN/UI THREAD
+
+        btnStartWithoutBackgroud.setOnClickListener {
+            try {
+                // simulate process compressing
+                for(i in 0..10){
+                    Thread.sleep(500)
+                    val persentage =  i * 10
+
+                    if(persentage == 100){
+                        tvStatus.setText(R.string.task_completed)
+                    }else{
+                        tvStatus.text = String.format(getString(R.string.compressing),persentage)
+                    }
+
+                }
+            } catch (e: InterruptedException){
+                e.printStackTrace()
+            }
+        }
+
+
+        /*
+        WITH BACKGROUND THREAD
+        1. EXECUTOR + HANDLER
+        */
 
         // Membuat sebuah thread baru
         val executor = Executors.newSingleThreadExecutor()
@@ -44,5 +78,29 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+        // 2. COROUTINE
+
+        btnStartCoroutine.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.Default){
+                //simulate process in background thread
+                for(i in 0..10){
+                    delay(500)
+                    val persentage = i * 10
+
+                    withContext(Dispatchers.Main){
+                        //update ui in main thread
+                        if(persentage ==  100){
+                            tvStatus.setText(R.string.task_completed)
+                        }else{
+                            tvStatus.text = String.format(getString(R.string.compressing), persentage)
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
 }
